012942a4ec026a83fbdc29549b1c94ce9128e62b
Elena Vnukova <e.vnukova@zonatelecom.ru>
2022-03-03 17:26:13 +0300

    new logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,68 +1,66 @@
 package logger
 
 import (
-	pkgerrors "github.com/pkg/errors"
+	"fmt"
+	"os"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-// stackTracer is errors interface from package 'github.com/pkg/errors'
-type stackTracer interface {
-	StackTrace() pkgerrors.StackTrace
+const (
+	loggerModeDev     = "dev"     // development logger mode
+	loggerModeProd    = "prod"    // production logger mode
+	loggerModeTesting = "testing" // logger mode for tests
+	defaultLoggerMode = loggerModeDev
+)
+
+var (
+	logger *zap.Logger
+
+	Trace func(msg string, fields ...zapcore.Field)
+	Info  func(msg string, fields ...zapcore.Field)
+	Warn  func(msg string, fields ...zapcore.Field)
+	Error func(msg string, fields ...zapcore.Field)
+	Fatal func(msg string, fields ...zapcore.Field)
+	Sugar *zap.SugaredLogger
+	Sync  func() error
+)
+
+func init() {
+	InitLogger(defaultLoggerMode)
 }
 
-// logger is a struct for loggers
-type logger struct {
-	logger    *zap.SugaredLogger // logger for common errors. Prints standart stacktrace
-	errLogger *zap.SugaredLogger // logger for errors from package 'github.com/pkg/errors'. Prints stracktrace saved in error
-}
-
-// initialize initializes loggers
-func (l *logger) initialize(loggerMode string) (func(), error) {
-	var err error
-	l.logger, err = newLogger(loggerMode, false)
-	if err != nil {
-		return nil, err
+func createLogger(loggerMode string) *zap.Logger {
+	var configEncoder zapcore.EncoderConfig
+	logLevel := zapcore.DebugLevel
+	if loggerMode == loggerModeProd { // logger for development mode
+		configEncoder = zap.NewProductionEncoderConfig()
+		logLevel = zapcore.InfoLevel
+	} else {
+		if loggerMode != loggerModeDev && len(loggerMode) > 0 {
+			fmt.Printf("wrong logger mode: %s, will use dev logger", loggerMode)
+		} else if len(loggerMode) == 0 {
+			fmt.Printf("logger mode is empty, will use dev logger")
+		}
+		configEncoder = zap.NewDevelopmentEncoderConfig()
 	}
-
-	l.errLogger, err = newLogger(loggerMode, true)
-	if err != nil {
-		return nil, err
-	}
-
-	return l.finish, nil
+	configEncoder.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	core := zapcore.NewCore(zapcore.NewJSONEncoder(configEncoder), os.Stdout, logLevel)
+	return zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.PanicLevel))
 }
 
-// finish is finalizer. It flushes any buffered log entries
-func (l *logger) finish() {
-	l.logger.Sync()
-	l.errLogger.Sync()
+func NewLogger(loggerMode string) *zap.Logger {
+	return createLogger(loggerMode)
 }
 
-// choose chooses logger by error type
-func (l *logger) choose(err error) *zap.SugaredLogger {
-	if _, ok := err.(stackTracer); ok {
-		return l.errLogger
-	}
-	return l.logger
-}
-
-// error logs error message with optional payload and additional data
-func (l *logger) error(err error, payload interface{}, keysAndValues ...interface{}) {
-	kvs := []interface{}{"payload", payload}
-	kvs = append(kvs, keysAndValues...)
-	l.choose(err).With(kvs...).Errorf("%+v", err)
-}
-
-// fatal logs error message with optional payload and additional data, then calls os.Exit(1)
-func (l *logger) fatal(err error, payload interface{}, keysAndValues ...interface{}) {
-	kvs := []interface{}{"payload", payload}
-	kvs = append(kvs, keysAndValues...)
-	l.choose(err).With(kvs...).Fatalf("%+v", err)
-}
-
-// Log implements go-kit Logger interface. It creates a log event from keyvals
-// See https://pkg.go.dev/github.com/go-kit/log#Logger for details
-func (l *logger) Log(keyvals ...interface{}) error {
-	l.logger.Error(keyvals...)
-	return nil
+func InitLogger(loggerMode string) {
+	logger = createLogger(loggerMode)
+	Trace = logger.Debug
+	Error = logger.Error
+	Info = logger.Info
+	Warn = logger.Warn
+	Fatal = logger.Panic
+	Sugar = logger.Sugar()
+	Sync = logger.Sync
 }
--- logger/params.go
+++
@@ -1,5 +0,0 @@
-package logger
-
-const LoggerModeDev = "dev"         // development logger mode
-const LoggerModeProd = "prod"       // production logger mode
-const LoggerModeTesting = "testing" // logger mode for tests
